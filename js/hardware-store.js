425dd48dff291305ac65b1f1e8facc0824c4ccea
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-20 11:11:48 -0500

    Create hardware-store.js

---
+++ js/hardware-store.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function($) {
+    function handleFavorites(itemId, favorite) {
+        $.ajax({
+            url: ajax_object.ajax_url,
+            method: 'POST',
+            data: {
+                action: 'handle_favorites',
+                item_id: itemId,
+                favorite: favorite
+            },
+            success: function(response) {
+                if (favorite) {
+                    alert(response + ' added to your favorites.');
+                } else {
+                    alert(response + ' removed from your favorites.');
+                }
+            }
+        });
+    }
+
+    function handleNotes(itemId, note) {
+        $.ajax({
+            url: ajax_object.ajax_url,
+            method: 'POST',
+            data: {
+                action: 'handle_notes',
+                item_id: itemId,
+                note: note
+            },
+            success: function(response) {
+                alert('Note updated successfully for ' + response + '.');
+            }
+        });
+    }
+
+    $(document).on('click', '.favorite-checkbox', function() {
+        var itemId = $(this).data('item-id');
+        var favorite = $(this).is(':checked');
+        handleFavorites(itemId, favorite);
+    });
+
+    $(document).on('click', '.save-note', function() {
+        var itemId = $(this).data('item-id');
+        var note = $('#note-' + itemId).val();
+        handleNotes(itemId, note);
+    });
+});
