30c8b75c646a52ac05dbd1d49dfb7d41ecfd30a3
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-21 16:13:51 -0500

    Add fave sc

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
-<? php
+<?
+
 function enqueue_hardware_store_scripts() {
     wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
     wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), null, true);
@@ -343,3 +344,142 @@ function search_items()
     echo $output;
     wp_die();
 }	
+
+function user_favorites_shortcode()
+{
+    $user_id = get_current_user_id();
+    if (!$user_id) {
+        return 'Please log in to view your favorite items.';
+    }
+
+    // Get the favorite items from user meta
+    $favorite_items = get_user_meta($user_id, 'favorite_item', false);
+
+    // If there are no favorite items
+    if (empty($favorite_items)) {
+        return '<p>No items have been marked as favorites.</p>';
+    }
+
+    // Query the favorite items
+    $args = array(
+        'post_type' => 'item',
+        'post__in' => $favorite_items,
+        'orderby' => 'title',
+        'order' => 'ASC',
+    );
+	
+	 $query = new WP_Query($args);
+
+    $output = '';
+    if ($query->have_posts()) {
+        $output .= '
+            <table class="table">
+                <thead>
+                    <tr>
+						<th class="th-image">Image</th>
+                        <th class="th-category">Category</th>
+                        <th class="th-title">Title</th>
+                        <th class="th-excerpt">Excerpt</th>
+                        <th class="th-content">Content</th>
+                        <th class="th-state">NY State</th>
+                        <th class="th-local">Local (non-NYC)</th>
+                        <th class="th-city">Local (NYC)</th>
+                        <th class="th-favorites">Favorites</th>
+                        <th class="th-notes">Notes</th>
+                    </tr>
+                </thead>
+                <tbody>';
+        while ($query->have_posts()) {
+            $query->the_post();
+			$categories = get_the_terms(get_the_ID(), 'item-category');
+            $category_names = array();
+            foreach ($categories as $category) {
+                $category_names[] = $category->name;
+            }
+            $category_list = implode(', ', $category_names);
+
+            $favorite = get_user_meta(get_current_user_id(), 'favorite_item', false);
+            $is_favorite = in_array(get_the_ID(), $favorite);
+
+            $user_note = get_user_meta(get_current_user_id(), 'note_item_' . get_the_ID(), true);
+
+  			$button_text = empty($user_note) ? "Add" : "Edit";
+			
+			$ny_state_taxable = get_field('ny_state_taxable');
+			$ny_state_taxable_class = '';
+
+			if ($ny_state_taxable === 'taxable') {
+				$ny_state_taxable_class = 'is-taxable';
+			} elseif ($ny_state_taxable === 'exempt') {
+				$ny_state_taxable_class = 'is-exempt';
+			}
+
+			$ny_state_taxable = ucfirst($ny_state_taxable);
+
+			$local_non_nyc = get_field('local_non_nyc');
+			$local_non_nyc_class = '';
+
+			if ($local_non_nyc === 'taxable') {
+				$local_non_nyc_class = 'is-taxable';
+			} elseif ($local_non_nyc === 'exempt') {
+				$local_non_nyc_class = 'is-exempt';
+			}
+
+			$local_non_nyc = ucfirst($local_non_nyc);
+			
+			$local_nyc = get_field('local_nyc');
+			$local_nyc_class = '';
+
+			if ($local_nyc === 'taxable') {
+				$local_nyc_class = 'is-taxable';
+			} elseif ($local_nyc === 'exempt') {
+				$local_nyc_class = 'is-exempt';
+			}
+
+			$local_nyc = ucfirst($local_nyc);
+			
+            $output .= '
+                <tr>
+					<td>' . get_the_post_thumbnail(get_the_ID(), 'thumbnail') . '</td>
+                    <td>' . $category_list . '</td>
+                    <td>' . get_the_title() . '</td>
+                    <td>' . get_the_excerpt() . '</td>
+                    <td>' . get_the_content() . '</td>
+                    <td class="ny-state ' . $ny_state_taxable_class . '">' . $ny_state_taxable . '</td>
+                    <td class="non-nyc ' . $local_non_nyc_class . '">' . $local_non_nyc . '</td>
+                    <td class="nyc ' . $local_nyc_class . '">' . $local_nyc . '</td>
+                    <td><input type="checkbox" data-item-id="' . get_the_ID() . '" class="favorite-checkbox" ' . ($is_favorite ? 'checked' : '') . '></td>
+                    <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#item-modal-' . get_the_ID() . '">' . $button_text . '</button></td>
+                </tr>
+                <div class="modal fade" id="item-modal-' . get_the_ID() . '" tabindex="-1" role="dialog" aria-labelledby="item-modal-label" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="item-modal-label">Notes for ' . get_the_title() . '</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <textarea rows="4" class="form-control" id="note-' . get_the_ID() . '">' . esc_textarea($user_note) . '</textarea>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary save-note" data-item-id="' . get_the_ID() . '">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ';
+        }
+        $output .= '</tbody></table>';
+    } else {
+        $output .= '<p>No items found in your favorites.</p>';
+    }
+	
+	 wp_reset_postdata();
+
+    return $output;
+}
+
+add_shortcode('user-favorites', 'user_favorites_shortcode');
