2e5f49e0f8f8050654d4a87718e459c8cdc9b0f3
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-23 05:54:41 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@ function enqueue_custom_styles_scripts() {
     wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'));
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array('jquery'), false, true);
 	
-	wp_localize_script('custom-script', 'script_vars', array('ajaxurl' => admin_url('admin-ajax.php')));
+	wp_localize_script('custom-script', 'ajax_object', array(
+		'ajax_url' => admin_url('admin-ajax.php'),
+		'nonce' => wp_create_nonce('ajax-items-table-call')
+	));
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles_scripts');
 
@@ -14,10 +17,10 @@ add_action('wp_enqueue_scripts', 'enqueue_custom_styles_scripts');
 add_action('wp_ajax_search_and_filter_items', 'search_and_filter_items');
 add_action('wp_ajax_nopriv_search_and_filter_items', 'search_and_filter_items');
 
-function search_and_filter_items() {
-    $search_term = $_POST['search_term'];
-    $item_category = $_POST['item_category'];
-    $first_letter = $_POST['first_letter'];
+function search_and_filter_items($initial_load = false) {
+    $search_term = isset($_REQUEST['search_term']) ? $_REQUEST['search_term'] : '';
+    $item_category = isset($_REQUEST['item_category']) ? $_REQUEST['item_category'] : 'all';
+    $first_letter = isset($_REQUEST['first_letter']) ? $_REQUEST['first_letter'] : 'all';
 
     $args = array(
         'post_type'      => 'item',
@@ -46,6 +49,12 @@ if ($first_letter != 'all') {
 }
 
 $query = new WP_Query($args);
+	
+if ($initial_load) {
+    ob_start();
+} else {
+    check_ajax_referer('ajax-items-table-call', '_wpnonce', true);
+}
 
 if ($query->have_posts()) {
     while ($query->have_posts()) {
@@ -56,8 +65,11 @@ if ($query->have_posts()) {
 }
 
 wp_reset_postdata();
-        
-    die;
+if ($initial_load) {
+        return ob_get_clean();
+    } else {
+        die;
+    }
 }
 
 // AJAX handler for mark item as favorite
@@ -93,83 +105,84 @@ function update_item_note() {
 }
 
 function items_table_shortcode($atts) {
-ob_start(); ?><!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
+        ob_start(); ?><!DOCTYPE html>
+        <html>
+        <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        </head>
+        <body>
 
-<input type="text" id="search_box" placeholder="Search items...">
-<select id="item_category_dropdown"><?php
-$item_categories = get_terms(array(
-    'taxonomy'   => 'item-category',
-    'hide_empty' => false
-));
-?>  <option value="all">All Categories</option><?php
-foreach ($item_categories as $item_category) {
-?>  <option value="<?php echo $item_category->term_id; ?>"><?php echo $item_category->name; ?></option><?php
+        <input type="text" id="search_box" placeholder="Search items...">
+        <select id="item_category_dropdown"><?php
+        $item_categories = get_terms(array(
+            'taxonomy'   => 'item-category',
+            'hide_empty' => false
+        ));
+        ?>  <option value="all">All Categories</option><?php
+        foreach ($item_categories as $item_category) {
+        ?>  <option value="<?php echo $item_category->term_id; ?>"><?php echo $item_category->name; ?></option><?php
+        }
+        ?></select>
+        <select id="first_letter_dropdown">
+            <option value="all">All</option><?php
+        foreach (range('A', 'Z') as $letter) {
+        ?>  <option value="<?php echo $letter; ?>"><?php echo $letter; ?></option><?php
+        }
+        ?></select>
+
+        <table id="items_table">
+            <thead>
+                <tr>
+                    <th class="featured-image">Featured Image</th>
+                    <th class="category">Category</th>
+                    <th class="title">Title</th>
+                    <th class="excerpt">Excerpt</th>
+                    <th class="content">Content</th>
+                    <th class="ny-state-taxable">NY State Taxable</th>
+                    <th class="favorites">Favorites</th>
+                    <th class="notes">Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php echo search_and_filter_items(true); ?>
+            </tbody>
+        </table>
+
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                jQuery('#search_box').trigger('input');
+            });
+        </script>
+
+        <div class="modal" tabindex="-1" role="dialog" id="note_modal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add/Edit Note</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" id="note_item_id">
+                <textarea id="note_editor"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" id="save_note">Save Note</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        </body>
+        </html><?php
+        return ob_get_clean();
 }
-?></select>
-<select id="first_letter_dropdown">
-    <option value="all">All</option><?php
-foreach (range('A', 'Z') as $letter) {
-?>  <option value="<?php echo $letter; ?>"><?php echo $letter; ?></option><?php
-}
-?></select>
 
-<table id="items_table">
-    <thead>
-        <tr>
-            <th class="featured-image">Featured Image</th>
-            <th class="category">Category</th>
-            <th class="title">Title</th>
-            <th class="excerpt">Excerpt</th>
-            <th class="content">Content</th>
-            <th class="ny-state-taxable">NY State Taxable</th>
-            <th class="favorites">Favorites</th>
-            <th class="notes">Notes</th>
-        </tr>
-    </thead>
-    <tbody>
-
-    </tbody>
-</table>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        jQuery('#search_box').trigger('input');
-    });
-</script>
-
-<div class="modal" tabindex="-1" role="dialog" id="note_modal">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Add/Edit Note</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <input type="hidden" id="note_item_id">
-        <textarea id="note_editor"></textarea>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id="save_note">Save Note</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-</body>
-</html><?php
-return ob_get_clean();
-}
 add_shortcode('items_table', 'items_table_shortcode');
 
 function favorites_table_shortcode($atts) {
