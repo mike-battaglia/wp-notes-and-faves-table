d94067720f829bf6f72e32e3225661ae759867cd
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-23 04:45:45 -0500

    Init T3

--- custom.js
+++ custom.js
@@ -1,1 +1,9 @@
-/*js goes here*/
+jQuery(document).ready(function($) {
+    
+    // Add your AJAX calls, UI update, and event listener logic for the below features:
+    // 1. Search
+    // 2. Filter
+    // 3. Mark item as favorites
+    // 4. Add/Edit private notes
+    
+});
