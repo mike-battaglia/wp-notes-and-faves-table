ab999d79ae219e2d7c46caf4f15b71c456d9a5ac
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-23 05:27:23 -0500

    0526

--- functions.php
+++ functions.php
@@ -1,10 +1,9 @@
 <?
-
 function enqueue_custom_styles_scripts() {
     wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'));
-    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/custom-scripts.js', array('jquery'), false, true);
+    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array('jquery'), false, true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles_scripts');
 
@@ -174,4 +173,3 @@ function favorites_table_shortcode($atts) {
     return ob_get_clean();
 }
 add_shortcode('favorites_table', 'favorites_table_shortcode');
-
