10658c335a5ad3504353c8c0c14fbe332cf1be64
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2023-05-23 05:32:20 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,12 @@
+<?
+
 function enqueue_custom_styles_scripts() {
     wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'));
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array('jquery'), false, true);
+	
+	wp_localize_script('custom-script', 'script_vars', array('ajaxurl' => admin_url('admin-ajax.php')));
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles_scripts');
 
